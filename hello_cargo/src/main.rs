3512804083f3d9fd6168c75609528fead31aa3b7
HaskMoney <heartstring85@gmail.com>
2023-01-02 13:47:21 -0800

    updated readme

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
     println!("Hello, world!");
 }
-// create project with 'cargo new'
+// create project with 'cargo new project_name'
 // build project with 'cargo build'
 // build and run proj with 'cargo run'
 // build proj without producing .exe: 'cargo check'
