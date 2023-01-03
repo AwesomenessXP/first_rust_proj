f6aaffae537bcaf145ac6ff07d1932cc85e5a5ca
HaskMoney <heartstring85@gmail.com>
2023-01-02 22:24:09 -0800

    added notes for string slices

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -5,7 +5,7 @@ fn main() {
     s.push_str(", world!"); // append a literal to a string
     println!("{s}");
 
-    // ---------- BASICS OF OWNERSHIP ------------
+    // ---------- BASICS OF OWNERSHIP ----------------------------------
 
     // integers are fixed, known sizes -> stored in stack
     let x = 5;
@@ -17,7 +17,8 @@ fn main() {
     let s2 = s1.clone();
     println!("s1 = {}, s2 = {}", s1, s2);
 
-    // --------- TRANSFERRING OWNERSHIP TO A DIFFERENT SCOPE-------
+
+    // --------- TRANSFERRING OWNERSHIP TO A DIFFERENT SCOPE-------------
     let s = String::from("hello"); // s is in scope
     takes_ownership(s); // s's value moved into function, no longer valid
 
@@ -33,7 +34,8 @@ fn main() {
     // s2 transfers ownership to s3, so does nothing
     // s3 transfers ownership, gets ownership again, then drops its value
 
-    // ----------- REFERENCES AND BORROWING ---------------------
+
+    // ----------- REFERENCES AND BORROWING ----------------------------
     let s1 = String::from("hello"); // s1 comes to scope
     // make a *reference* to s1, but not transfer ownership to a diff scope!
     let len = calculate_length(&s1); 
@@ -53,11 +55,77 @@ fn main() {
     let r2 = &mut s; // this is valid bc its in another scope
     println!("r2: {r2}");
 
-    // --------- DANGLING POINTERS ------------------
+
+    // --------- DANGLING POINTERS ------------------------------------
     let reference_to_nothing = dangle();
+
+    // --------------- SLICE TYPES ------------------------------------
+
+    // solution: string slice
+    let s = String::from("hello world");
+    // make 2 immutable references to s
+    let hello = &s[0..5];
+    let world = &s[6..11];
+
+    // range syntax
+    // these are the same: 
+    let slice = &s[0..2];
+
+    // if the slice contains last byte:
+    let len = s.len();
+    let slice = &s[3..len];
+    let slice = &s[3..]; // this is same as the last line!
+
+    // if the slice has the ENTIRE string:
+    let len = s.len();
+    let slice = &s[0..len];
+    let slice = &s[..]; // same as last line
+
+    // example:
+    let mut s = String::from("hello world");
+    let word = first_word(&s); // word will get the value 5
+    // s.clear(); // error! clear is a *mutable* reference
+    println!("the first word is: {}", word);
+
+    let my_string = String::from("hello world");
+
+    // `first_word` works on slices of `String`s, whether partial or whole
+    let word = first_word(&my_string[0..6]);
+    let word = first_word(&my_string[..]);
+    // `first_word` also works on references to `String`s, which are equivalent
+    // to whole slices of `String`s
+    let word = first_word(&my_string);
+
+    let my_string_literal = "hello world";
+
+    // `first_word` works on slices of string literals, whether partial or whole
+    let word = first_word(&my_string_literal[0..6]);
+    let word = first_word(&my_string_literal[..]);
+
+    // Because string literals *are* string slices already,
+    // this works too, without the slice syntax!
+    let word = first_word(my_string_literal);
+
+    // ----------- STRING LITERALS AS SLICES ------------------------
+    let s = "Hello, world!"; // here, s is &str -> so its immutable
+    // str can be String, but String CANNOT be str!
 }
 
-fn dangle() -> &String {
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes(); // convert string to array of bytes
+    // i = index, &item = byte in tuple
+    // iter() -> returns each element in a collection
+    // enumerate() -> returns an index, and value in a tuple -> (i, value)
+    for (i, &item) in bytes.iter().enumerate() {
+        // look for the first space in the string
+        if item == b' ' { // b' ' is an ASCII byte literal with a value of ' ' 
+            return &s[0..i];
+        }// if
+    }// for
+    &s[..]
+}
+
+fn dangle() -> String {
     let s = String::from("hello");
     // &s -> once s is out of scope, it references nothing!
     s
